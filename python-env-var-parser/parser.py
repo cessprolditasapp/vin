7a369355dd180944272c91e4f59248270ebba364
vinclv <vinod.chelladurai@sumup.com>
2022-06-23 14:56:35 +0200

    script and workflow to parse worklfow environment variable via python

---
+++ python-env-var-parser/parser.py
@@ -0,0 +1,11 @@
+import os
+
+
+try:
+    env_file = os.getenv('GITHUB_ENV')
+    with open(env_file, "a") as env_file:
+        env_file.write("ENV_VAR=1")
+    print(f"+++ Environment variable is set +++"")
+
+except Exception as e:
+    print(f"+++ Error in setting environment variable :: {e} +++")
