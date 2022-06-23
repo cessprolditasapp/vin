ca986bb4ec6099880083e935a36d67a46c28415d
vinclv <vinod.chelladurai@sumup.com>
2022-06-23 15:02:44 +0200

    script and workflow to parse worklfow environment variable via python

--- python-env-var-parser/parser.py
+++ python-env-var-parser/parser.py
@@ -8,4 +8,4 @@ try:
     print(f"+++ Environment variable is set +++"")
 
 except Exception as e:
-    print(f"+++ Error in setting environment variable :: {e} +++")
+    print(f"+++ Error in setting the environment variable :: {e} +++")
